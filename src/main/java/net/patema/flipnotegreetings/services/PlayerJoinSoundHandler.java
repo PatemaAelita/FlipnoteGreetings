0699b3e63bd18df5138321d8c8f20acaa863d83e
patema <25232455+PatemaAelita@users.noreply.github.com>
2024-07-17 16:06:51 +0200

    Fixed an issue where the sound was not played in multiplayer

--- src/main/java/net/patema/flipnotegreetings/services/PlayerJoinSoundHandler.java
+++ src/main/java/net/patema/flipnotegreetings/services/PlayerJoinSoundHandler.java
@@ -1,17 +1,26 @@
 package net.patema.flipnotegreetings.services;
 
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.text.Text;
 import net.patema.flipnotegreetings.sound.ModSounds;
 
 public class PlayerJoinSoundHandler {
     public static void register() {
-        ServerEntityEvents.ENTITY_LOAD.register((entity, serverWorld) -> {
-            if (entity instanceof ServerPlayerEntity player) {
-                player.playSound(ModSounds.STARTUP_SOUND, SoundCategory.PLAYERS, 1.0F, 1.0F);
-                player.sendMessage(Text.literal("frog"));
+        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
+            playSoundAndMessage(client);
+        });
+
+        ClientTickEvents.END_CLIENT_TICK.register(PlayerJoinSoundHandler::playSoundAndMessage);
+    }
+
+    private static void playSoundAndMessage(MinecraftClient client) {
+        client.execute(() -> {
+            if (client.player != null && client.player.age == 0) {
+                client.player.playSound(ModSounds.STARTUP_SOUND, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                client.player.sendMessage(Text.literal("frog!!!"), false);
             }
         });
     }
--- src/main/java/net/patema/flipnotegreetings/sound/ModSounds.java
+++ src/main/java/net/patema/flipnotegreetings/sound/ModSounds.java
@@ -4,16 +4,17 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
+import net.patema.flipnotegreetings.FlipnoteGreetings;
 
 public class ModSounds {
     public static final SoundEvent STARTUP_SOUND = registerSoundEvent("startup_sound");
 
     private static SoundEvent registerSoundEvent(String name) {
-        Identifier id = new Identifier(net.patema.flipnotegreetings.FlipnoteGreetings.MOD_ID, name);
+        Identifier id = new Identifier(FlipnoteGreetings.MOD_ID, name);
         return Registry.register(Registries.SOUND_EVENT, id, SoundEvent.of(id));
     }
 
     public static void registerSounds(){
-        net.patema.flipnotegreetings.FlipnoteGreetings.LOGGER.info("Registering sounds for "+ net.patema.flipnotegreetings.FlipnoteGreetings.MOD_ID);
+        net.patema.flipnotegreetings.FlipnoteGreetings.LOGGER.info("Registering sounds for "+ FlipnoteGreetings.MOD_ID);
     }
 }
